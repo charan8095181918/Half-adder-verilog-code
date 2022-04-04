bbe129a1a01b44170a931cd1509f5ee9fa5310dc
charan8095181918 <60030130+charan8095181918@users.noreply.github.com>
2022-04-04 12:05:11 +0530

    Add files via upload

---
+++ half-adeer.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:21:50 04/04/2022 
+// Design Name: 
+// Module Name:    half-adeer 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+`timescale 1 ns/10 ps
+module halfadder
+(
+   input a,b,
+   output  sum,carry
+);
+
+assign sum=a^b;
+assign carry=a&b;
+endmodule
+
---
+++ half_adder_txt.v
@@ -0,0 +1,47 @@
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   11:23:23 04/04/2022
+// Design Name:   halfadder
+// Module Name:   D:/exp/dsd/Charan/half_adder_txt.v
+// Project Name:  Charan
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: halfadder
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1 ns/10 ps
+module half_adder_txt();
+reg a,b;
+wire sum,carry;
+
+halfadder UUT(.a(a),.b(b),.sum(sum),.carry(carry));
+initial begin
+$monitor($time,"a=%b b=%b sum=%b carry=%b",a,b,sum,carry);
+a=0;
+b=0;
+#10
+a=0;
+b=1;
+#10
+a=1;
+b=0;
+#10
+a=1;
+b=1;
+#10;
+end
+endmodule
+
